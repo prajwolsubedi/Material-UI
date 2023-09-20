c4009a1444d85a4022462ed96ff7023fc3a5f48d
prajwol subedi <prajwolsubedizzz@gmail.com>
2023-09-20 16:41:31 +0545

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
     <>
       <h1>Hello</h1>
       <h1>World</h1>
+      <h1>Material ui</h1>
     </>
   );
-}
+};
 
 export default App;
